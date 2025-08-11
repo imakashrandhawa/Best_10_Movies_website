1d5975234b943922bea04a30cb80c973275de82a
Randhawa_codes <98721872+imakashrandhawa@users.noreply.github.com>
2025-08-10 22:46:38 -0230

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import os
 
 from flask import Flask, render_template, redirect, url_for, request
 from flask.cli import load_dotenv
-from bootstrap_flask import Bootstrap5
+from flask_bootstrap import Bootstrap5
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
 from sqlalchemy import Integer, String, Float, nullslast
